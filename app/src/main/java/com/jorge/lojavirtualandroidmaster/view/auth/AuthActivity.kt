7bcc9c8220871feb1cd64cbc2f3d6a50812de3ab
Antonio Jorge Garcia Tranin <antoniotraninjorge@gmail.com>
2024-01-27 15:22:09 -0400

    Initial commit

---
+++ app/src/main/java/com/jorge/lojavirtualandroidmaster/view/auth/AuthActivity.kt
@@ -0,0 +1,15 @@
+package com.jorge.lojavirtualandroidmaster.view.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jorge.lojavirtualandroidmaster.databinding.ActivityAuthBinding
+
+class AuthActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAuthBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
