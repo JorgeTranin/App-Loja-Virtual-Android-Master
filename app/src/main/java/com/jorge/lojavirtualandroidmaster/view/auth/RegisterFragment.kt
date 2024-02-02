0746e22d7a1c2a5d2dc250a676b2ec81db97bf5a
Antonio Jorge Garcia Tranin <antoniotraninjorge@gmail.com>
2024-02-02 13:27:09 -0400

    feat - add layout tela de login e cadastro de novos usuários

--- app/src/main/java/com/jorge/lojavirtualandroidmaster/view/auth/RegisterFragment.kt
+++ app/src/main/java/com/jorge/lojavirtualandroidmaster/view/auth/RegisterFragment.kt
@@ -1,26 +1,52 @@
 package com.jorge.lojavirtualandroidmaster.view.auth
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.jorge.lojavirtualandroidmaster.R
+import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
+import com.jorge.lojavirtualandroidmaster.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment() {
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        binding.btnCadastrar.setOnClickListener {
+
+            val check = checkFields()
+            if (check){
+                registerFirebase()
+            }
+        }
+    }
+
+    private fun registerFirebase() {
+
+    }
+
+    private fun checkFields(): Boolean {
+        if (binding.etEmail.text.isNullOrEmpty() || binding.etNome.text.isNullOrEmpty() || binding.etPassword.text.isNullOrEmpty()){
+            Snackbar.make(binding.root, "Preencha todos os campos" ,Snackbar.LENGTH_LONG).show()
+            return false
+        }else{
+            return true
+        }
     }
 
 
